cd4828134cec664738ecfcd387d025680bc2022d
dingnoni <rladnjs1218@naver.com>
2022-01-10 20:36:48 +0900

    learn 반복문

---
+++ ex13.c
@@ -0,0 +1,16 @@
+//백준 2439
+#include <stdio.h>
+
+int main() {
+    int i;
+    int j;
+    int n;
+    scanf("%d", &n);
+    for(i=1; i <=n; i++) {
+        for(j=1; j>=(n-i); j++) 
+            printf(" ");
+        for(j=1; j<=i; j++) 
+            printf("*");
+        printf("\n");
+    }
+}

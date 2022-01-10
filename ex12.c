690babdfa5131d402fde536f763adc913cd94335
dingnoni <rladnjs1218@naver.com>
2022-01-10 20:36:20 +0900

    learn 반복문

---
+++ ex12.c
@@ -0,0 +1,12 @@
+// 별 삼각형 만들기,, 이해가 잘 안됨 ..
+#include <stdio.h>
+
+int main() {
+    for(int i=1; i<=5; i++) {
+        for(int j=1; j<=i; j++) {
+            printf("*");
+        
+        }
+        printf("\n");
+    }
+}

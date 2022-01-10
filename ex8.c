535bace2b8c9572a28d5d3b69235ce175fd1b04d
dingnoni <rladnjs1218@naver.com>
2022-01-10 17:32:14 +0900

    calculate test grade

---
+++ ex8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if((100 >= n) && (90 <= n)) {
+        printf("A");
+    }
+
+    else if((89 >= n) && (80 <= n)) {
+        printf("B");
+    }
+    
+    else if((79 >= n) && (70 <= n)) {
+        printf("C");
+    }
+
+    else if((69 >= n) && (60 <= n)) {
+        printf("D");
+    }
+
+    else if((59 >= n) && (0 <= n)) {
+        printf("F");
+    }
+}

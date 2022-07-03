763acbf2eea8a20e53dee5259cab7149b145b81a
dingnoni <rladnjs1218@naver.com>
2022-07-03 20:07:15 +0900

    learn c restart!

---
+++ grade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    printf("enter your score: ");
+    scanf("%d", &score);
+
+    if ((score >= 90) && (score <= 100)) {
+        printf("Your grade is A\n");
+    }
+
+    else if ((score >= 80) && (score <= 89)) {
+        printf("Your grade is B\n");
+    }
+
+    else if ((score >= 70) && (score <= 79)) {
+        printf("Your grade is C\n");
+    }
+
+    else if ((score >= 60) && (score <= 69)) {
+        printf("Your grade is D\n");
+    }
+
+    else if ((score >= 0) && (score <= 59)) {
+        printf("Your grade is F\n");
+    }
+}
--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main() {
-    printf("Hello, World! \n");
-    return 0;
+
+void printArray(int input[], int len);
+
+void main() {
+    int arr1[3]={1,2,3};
+    int arr2[5]={1,2,3,4,5};
+
+    printArray(arr1,3);
+    printArray(arr2,5);
 }
-// Hello, World를 출력하는 프로그램
+
+void printArray(int input[], int len){
+    for(int i=0; i<len; i++)
+    printf("%d", input[i]);
+}
